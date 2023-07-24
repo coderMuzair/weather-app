79226645f8d73ceecc8fa4f04c931429784cb857
coderMuzair <coder.uzair@gmail.com>
2023-07-25 00:49:24 +0500

    final

--- app.js
+++ app.js
@@ -9,32 +9,30 @@ let humidity = document.querySelector('#humidity');
 let wind = document.querySelector('#wind');
 let submitBtn = document.querySelector('#submitBtn');
 let timeDiv = document.querySelector('#time');
-let timeStamp 
-let d = new Date();
-let hour = d.getHours();
-// let getHour = 1000 * 60 * 60;
-// let hour = time / getHour
-// console.log('time: ', hour);
 
 
 
-navigator.geolocation.getCurrentPosition((location)=> {
-    console.log('location: ', location);
-    let lat = location.coords.latitude;
-    let lon = location.coords.longitude;
-    // timeStamp = location.timestamp;
-    // console.log('timeStamp: ', new Date(timeStamp));
 
-    getweather(lat, lon)
 
-})
+// navigator.geolocation.getCurrentPosition((location)=> {
+//     console.log('location: ', location);
+//     let lat = location.coords.latitude;
+//     let lon = location.coords.longitude;
+//     // timeStamp = location.timestamp;
+//     // console.log('timeStamp: ', new Date(timeStamp));
 
-let getweather = (lat, lon)=>{
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8f319653e299b23b8910d5b21681dfab&units=metric`)
+//     getweather(lat, lon)
+
+// })
+
+let getweather = ()=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=4a3d866759f5c6c9a0a99313c6f40ef9&units=metric`)
     .then(res=> res.json())
     .then(data => showData(data))
     .catch(err=> console.log(err))
+    console.log("get weather")
 }
+getweather()
 
 cityInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
@@ -44,31 +42,26 @@ cityInput.addEventListener("keypress", function(event) {
   });
 
 let getWeatherbyUser = ()=>{
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=8f319653e299b23b8910d5b21681dfab&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=4a3d866759f5c6c9a0a99313c6f40ef9&units=metric`)
     .then(res=> res.json())
     .then(data => showData(data))
-    .catch(err=> console.log(err))
+    .catch(err=(err)=> {
+        Swal.fire({
+            icon: 'error',
+            title: 'City Not Found!',
+          })
+          console.log(err)
+    })
 }
 
 let showData = (data)=>{
-    console.log (data)
+ 
+    weatherImg.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     cityName.innerHTML = data.name + ", " + data.sys.country;
     temp.innerHTML = Math.round(data.main.temp) + "℃"
     feelsLike.innerHTML = Math.round(data.main.feels_like) + "℃"
     weatherMode.innerHTML = data.weather[0].main
     humidity.innerHTML = data.main.humidity + "%"
-    wind.innerHTML = data.wind.speed + " km/h"
-    timeDiv.innerHTML  = d;
-
-    let mode = data.weather[0].main;
-
-    
-    if(mode = "clear"){
-        console.log(hour)
-        if(hour>18 || hour<6 ){
-            weatherImg.src = "./images/clear-night.svg"
-        }else{
-            weatherImg.src = "./images/clear-day.svg"
-        }
-    }
+    wind.innerHTML = Math.round((data.wind.speed)*3.6) + " km/h"
+  
 }
